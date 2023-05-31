673bd0bea8f847620ca276326ef754d46eb5a4a8
Denis Ivanchenko <sicknature24@yandex.com>
2023-05-31 14:00:38 +0300

    feat: add solution for some promises-tasks

--- src/06-promises-tasks.js
+++ src/06-promises-tasks.js
@@ -56,8 +56,8 @@ function willYouMarryMe(isPositiveAnswer) {
  *    })
  *
  */
-function processAllPromises(/* array */) {
-  throw new Error('Not implemented');
+function processAllPromises(array) {
+  return Promise.all(array).then((values) => values);
 }
 
 /**
@@ -79,8 +79,8 @@ function processAllPromises(/* array */) {
  *    })
  *
  */
-function getFastestPromise(/* array */) {
-  throw new Error('Not implemented');
+function getFastestPromise(array) {
+  return Promise.race(array).then((value) => value);
 }
 
 /**
